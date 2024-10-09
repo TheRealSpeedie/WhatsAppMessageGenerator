1ccc18ab7ec8bb9e6f01f5d2275ed98ad02f790c
TheRealSpeedie <119527242+TheRealSpeedie@users.noreply.github.com>
2024-10-09 14:41:20 +0200

    Changed Design
    Changed Design and text to englisch

--- whatsapp_spamer/lib/main.dart
+++ whatsapp_spamer/lib/main.dart
@@ -5,27 +5,53 @@ import'dart:async';
 import 'package:flutter_native_contact_picker/flutter_native_contact_picker.dart';
 import 'package:flutter_native_contact_picker/model/contact.dart';
 
+const textColor = Color(0xFFdcf6e1);
+const backgroundColor = Color(0xFF030a03);
+const primaryColor = Color(0xFF8ce098);
+const primaryFgColor = Color(0xFF030a03);
+const secondaryColor = Color(0xFF213d7a);
+const secondaryFgColor = Color(0xFFdcf6e1);
+const accentColor = Color(0xFF574ace);
+const accentFgColor = Color(0xFFdcf6e1);
+
+const colorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  background: backgroundColor,
+  onBackground: textColor,
+  primary: primaryColor,
+  onPrimary: primaryFgColor,
+  secondary: secondaryColor,
+  onSecondary: secondaryFgColor,
+  tertiary: accentColor,
+  onTertiary: accentFgColor,
+  surface: backgroundColor,
+  onSurface: textColor,
+  error: Brightness.dark == Brightness.light ? Color(0xffB3261E) : Color(0xffF2B8B5),
+  onError: Brightness.dark == Brightness.light ? Color(0xffFFFFFF) : Color(0xff601410),
+);
+
 void main() {
-  runApp(MyApp());
+  runApp(WhatsAppMessageGenerator());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-  State<MyApp> createState() => _WhatsAppSpamerState();
+class WhatsAppMessageGenerator extends StatefulWidget {
+  const WhatsAppMessageGenerator({super.key});
+  @override
+  State<WhatsAppMessageGenerator> createState() => WhatsAppMessageGeneratorState();
 }
 
-class _WhatsAppSpamerState extends State<MyApp> {
+class WhatsAppMessageGeneratorState extends State<WhatsAppMessageGenerator> {
   final howOftenMessageSent = TextEditingController();
   final textToSend = TextEditingController();
   final FlutterNativeContactPicker _contactPicker =
   FlutterNativeContactPicker();
   Contact? contact;
-  String phoneNumber = "noch nicht ausgewählt";
+  String phoneNumber = "not selected yet";
   String finalTextToSend = "";
-  String contactName = "noch nicht ausgewählt";
+  String contactName = "not selected yet";
 
 
-  Future<void> _openWhatsAppLink() async {
+  Future<void> OpenWhatsAppLink() async {
     Multiplytext();
     final link = WhatsAppUnilink(
       phoneNumber: phoneNumber,
@@ -45,9 +71,22 @@ class _WhatsAppSpamerState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: colorScheme,
+        appBarTheme: AppBarTheme(
+          backgroundColor: primaryColor,
+          foregroundColor: primaryFgColor,
+        ),
+        scaffoldBackgroundColor: backgroundColor,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: textColor),
+          bodyMedium: TextStyle(color: textColor),
+          bodySmall: TextStyle(color: textColor),
+        ),
+      ),
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Your Whatsapp Spamer'),
+        title: Text('Your Whatsapp Message Generator'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -55,64 +94,59 @@ class _WhatsAppSpamerState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
-              controller: howOftenMessageSent,
-              keyboardType: TextInputType.number,
+              controller: textToSend,
+              keyboardType: TextInputType.text,
               decoration: InputDecoration(
-                labelText: 'Wie oft soll die Nachricht gesendet werden',
+                labelText: 'Text you want to send',
                 border: OutlineInputBorder(),
               ),
             ),
             SizedBox(height: 16),
             TextField(
-              controller: textToSend,
-              keyboardType: TextInputType.text,
+              controller: howOftenMessageSent,
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                labelText: 'Text der gesendet werden soll',
+                labelText: 'How often should the message be sent',
                 border: OutlineInputBorder(),
               ),
             ),
+            SizedBox(height: 16),
            ElevatedButton(
               onPressed: () async {
                 contact = await _contactPicker.selectContact();
                 setState(() {
-                  phoneNumber = contact!.phoneNumbers!.isNotEmpty ? contact?.phoneNumbers?.first as String : "[nummer]";
+                  phoneNumber = contact!.phoneNumbers!.isNotEmpty ? contact?.phoneNumbers?.first as String : "unknown number";
                 });
                 if (contact != null) {
                   setState(() {
-                    contactName = contact?.fullName ?? "[unbekannter Name]";
+                    contactName = contact?.fullName ?? "unknown Name";
                   });
                 } else {
                   setState(() {
-                    contactName = "kein Kontakt ausgewählt";
+                    contactName = "no Contact selected";
                   });
                 }
               },
-              child: Text('Selektiere den Kontakt'),
+              child: Text('Select a Contact'),
             ),
             SizedBox(height: 16),
-            Text('Der ausgewählte Kontakt:'),
+            Text('Selected Contact'),
             SizedBox(height: 5),
-            Text('Name: $contactName', key: Key("nameKey")),
-            Text('Nummer: $phoneNumber', key: Key("numberKey")),
+            Text('$contactName', key: Key("nameKey")),
+            Text('$phoneNumber', key: Key("numberKey")),
             SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                _openWhatsAppLink();
-              },
-              child: Text('Whatsapp Nachricht senden'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
+                OpenWhatsAppLink();
                 setState(() {
-                  phoneNumber = "noch nicht ausgewählt";
-                  contactName = "noch nicht ausgewählt";
+                  phoneNumber = "not selected yet";
+                  contactName = "not selected yet";
                   contact = null;
                 });
                 howOftenMessageSent.text = "";
                 textToSend.text = "";
               },
-              child: Text('Reset'),
+              child: Text('send Message'),
             )
           ],
         ),
